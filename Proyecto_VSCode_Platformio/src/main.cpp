e1f7921f595bd1512398b68949776e9a83b629f8
Matias Mansilla <103467306+matimansi@users.noreply.github.com>
2024-06-24 20:26:00 -0300

    Cambio de comentarios y estructura general

--- Proyecto_VSCode_Platformio/src/main.cpp
+++ Proyecto_VSCode_Platformio/src/main.cpp
@@ -1,25 +1,37 @@
+/*
+	LIBRERIAS o BIBLIOTECAS
+*/
+
 #include <Arduino.h>
 #include <SPI.h>
-#include <TFT_eSPI.h> // Hardware-specific library
+#include <TFT_eSPI.h>		//Libreria especifica del Hardware
 
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+TFT_eSPI tft = TFT_eSPI();	// Incluimos o Invocamos libreria custon
 
-#define TFT_GREY 0x5AEB // New colour
+//#define TFT_GREY 0x5AEB // New colour
 
-#define TFT_DC   18  // Data Command control pin - must use a pin in the range 0-31  (LCD_RS on my display)
-#define TFT_CS   33  // Chip select control pin
-#define TFT_RST  32  // Reset pin
+/*
+	DEFINICIONES DE PINES
+*/
+
+#define TFT_DC   18
+#define TFT_CS   33
+#define TFT_RST  32
 #define TFT_RD    2
-#define TFT_WR    4  // Write strobe control pin - must use a pin in the range 0-31
+#define TFT_WR    4
 
-#define TFT_D0   12  // Must use pins in the range 0-31 for the data bus
-#define TFT_D1   13  // so a single register write sets/clears all bits
+#define TFT_D0   12
+#define TFT_D1   13
 #define TFT_D2   26
 #define TFT_D3   25
 #define TFT_D4   21
 #define TFT_D5   5
 #define TFT_D6   27
 #define TFT_D7   14
+
+/*
+	FUENTES DE TEXTO
+*/
 
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
@@ -31,54 +43,99 @@ TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
 #define SMOOTH_FONT
 
-void setup() {
-  tft.init();
-  tft.setRotation(1);
+void setup()
+{
+	tft.init();
+	tft.setRotation(1);
 }
 
-void loop() {
-  // Fill screen with random colour so we can see the effect of printing with and without 
-  // a background colour defined
-  tft.fillScreen(random(0xFFFF));
-  
-  // Set "cursor" at top left corner of display (0,0) and select font 2
-  // (cursor will move to next line automatically during printing with 'tft.println'
-  //  or stay on the line is there is room for the text with tft.print)
-  tft.setCursor(0, 0, 2);
-  // Set the font colour to be white with a black background, set text size multiplier to 1
-  tft.setTextColor(TFT_WHITE,TFT_BLACK);  tft.setTextSize(1);
-  // We can now plot text on screen using the "print" class
-  tft.println("Hello World!");
-  
-  // Set the font colour to be yellow with no background, set to font 7
-  tft.setTextColor(TFT_YELLOW); tft.setTextFont(7);
-  tft.println(1234.56);
-  
-  // Set the font colour to be red with black background, set to font 4
-  tft.setTextColor(TFT_RED,TFT_BLACK);    tft.setTextFont(4);
-  tft.println((long)3735928559, HEX); // Should print DEADBEEF
+void loop()
+{
+	// Rellena la pantalla con un color random
+	tft.fillScreen(random(0xFFFF));
 
-  // Set the font colour to be green with black background, set to font 4
-  tft.setTextColor(TFT_GREEN,TFT_BLACK);
-  tft.setTextFont(4);
-  tft.println("Groop");
-  tft.println("I implore thee,");
+	/*
+		Seteamos el "cursor" en la esquina superior izquierda dek display -> [ (0, 0 ], 2)
+		Seleccionamos la fuente 2 -> (0, 0 [, 2) ]		
+	*/
 
-  // Change to font 2
-  tft.setTextFont(2);
-  tft.println(F("my foonting turlingdromes.")); // Can store strings in FLASH to save RAM
-  tft.println("And hooptiously drangle me");
-  tft.println("with crinkly bindlewurdles,");
-  // This next line is deliberately made too long for the display width to test
-  // automatic text wrapping onto the next line
-  tft.println("Or I will rend thee in the gobberwarts with my blurglecruncheon, see if I don't!");
-  
-  // Test some print formatting functions
-  float fnumber = 123.45;
-   // Set the font colour to be blue with no background, set to font 4
-  tft.setTextColor(TFT_BLUE);    tft.setTextFont(4);
-  tft.print("Float = "); tft.println(fnumber);           // Print floating point number
-  tft.print("Binary = "); tft.println((int)fnumber, BIN); // Print as integer value in binary
-  tft.print("Hexadecimal = "); tft.println((int)fnumber, HEX); // Print as integer number in Hexadecimal
-  delay(10000);
+	tft.setCursor(0, 0, 2);
+
+	// Setemos el color de la fuente a blanco con un fondo negro
+	tft.setTextColor(TFT_WHITE,TFT_BLACK);
+	
+	// Seteamos el multiplicador del tamaño del texto a 1
+	tft.setTextSize(1);
+
+	// Ahora podemos imprimir el texto en la pantalla
+	tft.println("Hello World!");
+	
+	/*
+		Seteamos el color de la fuente sin fondo
+		Elegimos la fuente 7
+		Escribimos en la pantalla
+	*/
+
+	tft.setTextColor(TFT_YELLOW);
+	tft.setTextFont(7);
+	tft.println(1234.56);
+
+	/*
+		Seteamos el color de la fuente a rojo con fondo negro
+		Elegimos la fuente 4
+		Escribimos en la pantalla
+	*/
+
+	tft.setTextColor(TFT_RED,TFT_BLACK);
+	tft.setTextFont(4);
+	tft.println((long)3735928559, HEX);		// Deberia imprimir DEADBEEF
+
+	/*
+		Seteamos el color de la fuente a verde con un fondo negro
+		Elgimos la fuente 4
+		Escribimos 2 lineas en la pantalla
+	*/
+
+	tft.setTextColor(TFT_GREEN,TFT_BLACK);
+	tft.setTextFont(4);
+	tft.println("Groop");
+	tft.println("I implore thee,");
+
+	/*
+		Cambiamos a la fuente 2
+	*/
+
+	tft.setTextFont(2);
+
+	tft.println(F("my foonting turlingdromes.")); // Podemos guardar strings en la memoria flahs para ahorrar RAM
+	tft.println("And hooptiously drangle me");
+	tft.println("with crinkly bindlewurdles,");
+
+	/*
+		La siguiente linea es muy larga para el ancho del dysplay
+		Automaticamente el texto se acomodo en la siguiente linea
+	*/
+	
+	tft.println("Or I will rend thee in the gobberwarts with my blurglecruncheon, see if I don't!");
+	
+	// Probemos algunas formas de formato de impresion
+	
+	float fnumber = 123.45;
+
+	/*
+		Seteamos el color de la fuente a azul sin fondo
+		Elegimos la fuente 4
+	*/
+	
+	tft.setTextColor(TFT_BLUE);
+	tft.setTextFont(4);
+
+	tft.print("Float = ");
+	tft.println(fnumber);				// Print floating point number
+	tft.print("Binary = ");
+	tft.println((int)fnumber, BIN);		// Print as integer value in binary
+	tft.print("Hexadecimal = ");
+	tft.println((int)fnumber, HEX);		// Print as integer number in Hexadecimal
+	
+	delay(10000);
 }
