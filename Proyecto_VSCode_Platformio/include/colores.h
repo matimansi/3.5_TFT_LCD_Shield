64c57ca544ddcfe7955fec1e5e01676c8a0b8374
Matias Mansilla <103467306+matimansi@users.noreply.github.com>
2024-06-24 21:31:47 -0300

    Nuevos archivos .h

---
+++ Proyecto_VSCode_Platformio/include/colores.h
@@ -0,0 +1,11 @@
+
+#define NEGRO       0x0000
+#define AZUL        0x001F
+#define VERDE       0x07E0
+#define MARRON      0x7800
+#define VIOLETA     0x780F
+#define ROJO        0xF800
+#define ROSA        0xF81F
+#define NARANJA     0xFD20
+#define AMARILLO    0xFFE0
+#define BLANCO      0xFFFF
---
+++ Proyecto_VSCode_Platformio/include/pines.h
@@ -0,0 +1,17 @@
+
+// Pines del 3.5" TFT LCD Shield
+
+#define TFT_DC   18
+#define TFT_CS   33
+#define TFT_RST  32
+#define TFT_RD    2
+#define TFT_WR    4
+
+#define TFT_D0   12
+#define TFT_D1   13
+#define TFT_D2   26
+#define TFT_D3   25
+#define TFT_D4   21
+#define TFT_D5   5
+#define TFT_D6   27
+#define TFT_D7   14
